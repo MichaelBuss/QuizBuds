09488762e242caedb9178457d9f2ce4017a5669c
Michael Buss Andersen <michaelbussandersen@gmail.com>
2018-12-27 09:05:22 +0100

    Better Flow Layout

--- Backies/ColumnFlowLayout.swift
+++ Backies/ColumnFlowLayout.swift
@@ -15,7 +15,13 @@ class ColumnFlowLayout: UICollectionViewFlowLayout {
         
         guard let cv = collectionView else { return }
         
-        self.itemSize = CGSize(width: cv.bounds.inset(by: cv.layoutMargins).size.width, height: 90)
+        let avaibleWidth = cv.bounds.inset(by: cv.layoutMargins).size.width
+        
+        let minColumnWidth = CGFloat(300.0)
+        let maxNumColumns = Int(avaibleWidth / minColumnWidth)
+        let cellWidth = (avaibleWidth / CGFloat(maxNumColumns)).rounded(.down)
+        
+        self.itemSize = CGSize(width: cellWidth, height: 90.0)
         
         self.sectionInset = UIEdgeInsets(top: self.minimumInteritemSpacing, left: 0.0, bottom: 0.0, right: 0.0)
         self.sectionInsetReference = .fromSafeArea
